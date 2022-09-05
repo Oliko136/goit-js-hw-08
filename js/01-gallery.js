a5b3d24a58227797d082e5b81a610ff60e610894
Oliko136 <olchick136@gmail.com>
2022-09-05 17:31:56 +0400

    Task 1 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery');
+
+renderGallery(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery__item', {captionsData: 'alt', captionDelay: 250});
+
+function renderGallery(items) {
+    const markup = items.map(({ preview, original, description }) =>
+        `<div><a class="gallery__item" href=${original}>
+            <img class="gallery__image" src=${preview} alt=${description} />
+        </a></div>`
+
+    ).join('');
+
+    gallery.insertAdjacentHTML('beforeend', markup);
+}
 console.log(galleryItems);
