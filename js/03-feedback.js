9deb5f8408e2359a9e00ab1b36c9e7a9eb56d7cf
Oliko136 <olchick136@gmail.com>
2022-09-05 19:24:36 +0400

    Task 3 completed

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateTextarea();
+
+
+function onFormInput(evt) {
+    const formData = {
+        email: refs.email.value,
+        message: refs.message.value,
+    }
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    const parsedData = JSON.parse(savedData);
+
+    if (savedData) {
+        refs.email.value = parsedData.email;
+        refs.message.value = parsedData.message;
+    }
+    
+}
