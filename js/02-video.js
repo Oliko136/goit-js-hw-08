b5293ec9c5659ba9298942c05a1395fbf31f87ca
Oliko136 <olchick136@gmail.com>
2022-09-06 18:03:30 +0400

    Task 2 completed

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+onReload();
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+
+function onTimeUpdate(evt) {
+    localStorage.setItem(STORAGE_KEY, evt.seconds);
+}
+
+function onReload() {
+    const savedTime = localStorage.getItem(STORAGE_KEY);
+    player.setCurrentTime(savedTime);
+}
+
+
+
+
 
